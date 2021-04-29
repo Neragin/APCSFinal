6c478122f3a5f15397698925d947ca1f2da243c6
kaustubh <kaustubh.khulbe@gmail.com>
2021-04-29 16:55:59 -0700

    trying stuff

--- app/src/main/java/com/example/fulkscord/SignUp.java
+++ app/src/main/java/com/example/fulkscord/SignUp.java
@@ -51,43 +51,41 @@ public class SignUp extends AppCompatActivity {
             password = (passwordView).getText().toString().trim();
             System.err.println("Name: " + username + ", email: " + email + ", pwd: " + password);
 
-            if(email == null || !email.matches(
+            if (email == null || !email.matches(
                     "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
-            ) || email.equals("")){
+            ) || email.equals("")) {
 //                Toast toast = Toast.makeText(this, Html.fromHtml("<font color='#ff0000' ><b>" + "Please use a valid email." + "</b></font>"));
 //                toast.show();
 //                emailView.setHint("Email Address");
 //                emailView.setText("");
-            }
-
-            else if(username == null || !username.matches("^[A-Za-z]\\w{5,29}$")) {
+            } else if (username == null || !username.matches("^[A-Za-z]\\w{5,29}$")) {
                 Toast toast = Toast.makeText(this, Html.fromHtml("<font color='#ff0000' ><b>" + "Your username must be between 6 to 30 characters, start with a letter, and only contain alphanumeric characters and underscores." + "</b></font>"), Toast.LENGTH_LONG);
                 toast.show();
                 usernameView.setHint("Username");
                 usernameView.setText("");
-            }
-
-            else if(password == null || !password.matches("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$")) {
+            } else if (password == null || !password.matches("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$")) {
                 Toast toast = Toast.makeText(this, "Please enter a valid password that: \n is more than eight characters and contains at least 1 letter and 1 number.", Toast.LENGTH_LONG);
                 toast.show();
                 passwordView.setHint("Password");
                 passwordView.setText("");
-            }
-            else {
+            } else {
 
                 boolean exists = false;
+                FirebaseDatabase mFirebaseInstance;
 
-                Query queries = mDatabase.child(DatabaseKeys.userKey).orderByChild("username").equalTo(username);
-                System.out.println(queries);
+                mFirebaseInstance = FirebaseDatabase.getInstance();
 
-                ValueEventListener eventListener = new ValueEventListener() {
+                mFirebaseInstance.getReference().child("users").child(username).addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
-                        System.out.println("hello");
-                        if(!dataSnapshot.exists()) {
+                        if (dataSnapshot != null && dataSnapshot.getValue() != null) {
+                            // username is already taken
+                        } else {
+                            // username is valid
                             User user = new User(username, email, "1231231234", password);
                             System.out.println("hello");
                             mDatabase.child(DatabaseKeys.userKey).child(username).setValue(user);
+
                         }
                     }
 
@@ -95,20 +93,8 @@ public class SignUp extends AppCompatActivity {
                     public void onCancelled(DatabaseError databaseError) {
                         System.out.println("cancelled");
                     }
-                };
-
-                queries.addListenerForSingleValueEvent(eventListener);
-
+                });
             }
-
         });
-
-//        if(email == null || email.equals("")) {
-//
-//        }
-
-        //EMAIL VERIFICATION REGEX FROM ONLINE
-        //if (username == null || username.equals(""))
-
     }
 }
