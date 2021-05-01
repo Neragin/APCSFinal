2606ae5866be94426dda1e6caf190830353fe55b
kaustubh <kaustubh.khulbe@gmail.com>
2021-05-01 16:11:16 -0700

    recycler view for dm done

--- app/src/main/java/com/example/fulkscord/directMessage/DirectMessageActivity.java
+++ app/src/main/java/com/example/fulkscord/directMessage/DirectMessageActivity.java
@@ -1,4 +1,16 @@
 package com.example.fulkscord.directMessage;
 
-public class DirectMessageActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fulkscord.R;
+
+public class DirectMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_direct_message);
+    }
 }
